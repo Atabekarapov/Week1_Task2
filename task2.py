cf4590cccb4f6d479d4e85814c82fc002485b525
Atabek <atabekarapov14@gmail.com>
2020-11-01 17:28:14 +0600

    first

---
+++ task2.py
@@ -0,0 +1,30 @@
+# Homework
+# # 1)
+a = "Hello"
+b = "world, "
+c = "I'm, "
+name = "John! "
+full_name = a + " " + b + c + name
+print(full_name)
+
+# # 2)
+a = "Hello "
+result = a * 3
+print(result)
+
+#  3)
+name = "John Snow!"
+a = "Hello, "
+result = a + name
+print(result)
+
+# 4)
+a = "Hello"
+print(a[::-1])
+
+
+
+
+
+
+
